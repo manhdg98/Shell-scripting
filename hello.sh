eca8f5b78e2eaef972eca30dc4092599e10b3cf7
Tahuynh <manhdg98@gmail.com>
2021-07-01 20:21:58 +0700

    run and read input

---
+++ hello.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+echo "Hello World"
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD # Directory current working
+
+name=Manh
+echo The name is $name
---
+++ readname.sh
@@ -0,0 +1,10 @@
+# bin/bash
+
+# echo "Enter names : "
+# read name1 name2 name3
+# echo "Enterd name : $name1, $name2, $name3"
+
+read -p 'user name: ' user_name
+read -sp 'password: ' password
+
+echo 'user name: ' $user_name + 'pass word: ' $password
